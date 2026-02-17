e6a730821a38583749be48c50bb90615cc3f6f16
quinten-raymaekers_DPGMEDIA <raymaekersq@gmail.com>
2026-02-17 20:00:33 +0100

    Added search dtos

---
+++ src/main/java/com/omi/common/dto/search/PrescriptionSearchFlags.java
@@ -0,0 +1,12 @@
+package main.java.com.omi.common.dto.search;
+
+public record PrescriptionSearchFlags(
+        boolean isNarcotic,
+        boolean isReserveAntibiotic,
+        boolean isTprescriptionDrug,
+        boolean hasPrescriptionRestrictionAnnexIii,
+        boolean hasPrescriptionExclusionAnnexIii,
+        boolean flagArv,
+        boolean flagHasDiscount
+) {
+}
---
+++ src/main/java/com/omi/common/dto/search/PrescriptionSearchResult.java
@@ -0,0 +1,22 @@
+package main.java.com.omi.common.dto.search;
+
+import java.math.BigDecimal;
+
+public record PrescriptionSearchResult(
+        String pzn,
+        String packageName,
+        String manufacturerName,
+        String dosageForm,
+        String packageSizeByDecree,
+        BigDecimal packageQuantity,
+        String packageUnit,
+        BigDecimal pharmacyRetailPrice,
+        BigDecimal copaymentAmount,
+        String priceVsMaxReimbursement,
+        String wirkstoff,
+        String wirkstaerke,
+        String atcCode,
+
+        PrescriptionSearchFlags flags
+) {
+}
