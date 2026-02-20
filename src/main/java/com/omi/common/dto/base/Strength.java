65902b177629d245169ba7f4a7024ef1b391cca3
quinten-raymaekers_DPGMEDIA <raymaekersq@gmail.com>
2026-02-20 14:54:02 +0100

    fix/Strength-issue-fix

--- src/main/java/com/omi/common/dto/base/Strength.java
+++ src/main/java/com/omi/common/dto/base/Strength.java
@@ -3,9 +3,11 @@ package com.omi.common.dto.base;
 import com.omi.common.enums.generic.Unit;
 import lombok.Builder;
 
+import java.math.BigDecimal;
+
 @Builder
-public class Strength {
-    Double from;
-    Double to;
-    Unit unit;
-}
+public record Strength(
+    BigDecimal from,
+    BigDecimal to,
+    Unit unit
+) {}
